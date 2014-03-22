41293babbc540ca548fa3f8f62fe715326e13fb7
johnlucia <johnmlucia@gmail.com>
2014-03-22 11:13:05 -0700

    viewport and dynamic twitter handle

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,10 +1,8 @@
 class TweetsController < ApplicationController
-  require "rubygems"
-  require "twitter"
-  require 'twitter/geo'
   def index
-    @user = Twitter.user("johnlucia")
-    @tweets = Twitter.user_timeline("johnlucia")
+    twitter_handle = params[:handle] || "johnlucia"
+    @user = Twitter.user(twitter_handle)
+    @tweets = Twitter.user_timeline(twitter_handle)
     @tweets.reject! {|tweet| tweet.geo.nil? }
   end
 end
